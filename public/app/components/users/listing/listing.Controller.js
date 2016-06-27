56cc9ac42c51c14f7fa6c0fcbdf50ce9cc517aa2
root <root@localhost.localdomain>
2016-06-27 14:01:02 +0100

    adding html layout for listing users and grabing Users data from the api in  listingController.js

--- public/app/components/users/listing/listing.Controller.js
+++ public/app/components/users/listing/listing.Controller.js
@@ -1,7 +1,15 @@
 
 
-app.controller('UsersListingController', [function() {
-
+app.controller('UsersListingController', [ '$scope', '$http', function($scope, $http) {
+  $scope.UserList = [];
+  $http({
+  method: 'GET',
+  url: 'http://localhost:3000/api/users/json'
+}).then(function successCallback(res) {
+  $scope.UserList = res.data.data;
+  }, function errorCallback(res) {
+    console.log(res);
+  });
 
 
 }]);
